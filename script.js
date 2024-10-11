79305cc1e75cfe027fb60312530bf8a8c1e28e36
eneuendorf-briya <eneuendorf@briya.org>
2024-10-11 15:46:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-    const circle = document.getElementById('circle');
-    //const step1 = document.getElementById('step1');
-        //const square1 = document.getElementById('square1');
-        //const square2 = document.getElementById('square2');
-        //const square3 = document.getElementById('square3');
-
-        let circleClicked = false;
+const circle = document.getElementById('circle');
+const buffer = document.getElementById('buffer');
+let circleClicked = false;
     
-        // Circle click event
-        circle.addEventListener('click', () => {
+// Circle click event
+circle.addEventListener('click', () => {
+  if (!circleClicked) {
+    // Move the circle down to the bottom of the page
+    circle.style.top = '180%';
+    circle.style.left = '50%';
+    //circle.style.transform = 'translatey(20%)';
+    circleClicked = true;
+
+  }
+});
+
+buffer.addEventListener('click', () => {
           if (!circleClicked) {
             // Move the circle down to the bottom of the page
             circle.style.top = '180%';
-            circle.style.left = '50%';
-            circle.style.bottom = '20%';
             //circle.style.transform = 'translatey(20%)';
             circleClicked = true;
 
