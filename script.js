bfa6457a3a85fb1fedcf7ddc87452e3e86367235
eneuendorf-briya <eneuendorf@briya.org>
2024-10-16 11:51:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const circle = document.getElementById('circle');
 const buffer = document.getElementById('buffer');
-let circleClicked = false;
+let circleClicked1 = false;
+let circleClicked2 = false;
     
 // Circle click event
 circle.addEventListener('click', () => {
-  if (!circleClicked) {
+  if (!circleClicked1) {
+    circle.style.left = '75%';
+    circle.style.top = '20%';
+      
+    circleClicked1 = true;
+  } else {
     // Move the circle down to the bottom of the page
     circle.style.top = '180%';
     circle.style.left = '50%';
     //circle.style.transform = 'translatey(20%)';
     //buffer.style.transform = 'translatey(200%)';
     buffer.style.top = '220%';
-    circleClicked = true;
-
+    
+    circleClicked1 = true;
   }
 });
