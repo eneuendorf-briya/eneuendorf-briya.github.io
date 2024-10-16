1fe920efd700658b78c2220e25144a5a62badd7d
eneuendorf-briya <eneuendorf@briya.org>
2024-10-16 14:07:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 const circle = document.getElementById('circle');
 const buffer = document.getElementById('buffer');
+const step1 = document.getElementById('step1');
+const step2= document.getElementById('step2');
+const step3 = document.getElementById('step3');
+const step4 = document.getElementById('step4');
 let circleClicked1 = false;
 let circleClicked2 = false;
     
 // Circle click event
 circle.addEventListener('click', () => {
   if (!circleClicked1) {
-    circle.style.left = '75%';
+		//Shift the circle to the right
+		circle.style.left = '75%';
     circle.style.top = '20%';
+    step1.style.display = 'none';
+    step2.style.display = 'block';
       
     circleClicked1 = true;
-  } else {
+  } else if (!circleClicked2) {
     // Move the circle down to the bottom of the page
     circle.style.top = '180%';
     circle.style.left = '50%';
-    //circle.style.transform = 'translatey(20%)';
-    //buffer.style.transform = 'translatey(200%)';
     buffer.style.top = '220%';
+    step2.style.display = 'none';
+    step3.style.display = 'block';
+    step4.style.display = 'block';
     
-    circleClicked1 = true;
+    circleClicked2 = true;
+  } else {
+  	circle.style.top = '50%';
+    circle.style.left = '50%';
+    step3.style.display = 'none';
+    step4.style.display = 'none';
   }
 });
