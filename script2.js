f033686e98b779c7993a4d908cb0141f678a2cb6
eneuendorf-briya <eneuendorf@briya.org>
2024-10-16 15:34:54 -0400

    Update script2.js

--- script2.js
+++ script2.js
@@ -3,16 +3,11 @@ const step6 = document.getElementById('step6');
 let circleClicked1 = false;
     
 // Circle click event
-circle.addEventListener('click', () => {
-  if (!circleClicked1) {
-  	step5.style.display = 'none';
-    circle.style.transition = 'all 2s ease';
+  	step5.style.display = 'block';
+    circle.style.transition = 'all 3s ease';
     circle.style.top = '0%';
     circle.style.left = '20%';
     circle.style.width = '0px';
     circle.style.height = '0px';
-    
-    window.open('https://eneuendorf-briya.github.io/page2.html', '_blank');
-  }
-  
+
 });
