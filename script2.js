c9ff31316779a017cf96962733fc35004f56ab64
eneuendorf-briya <eneuendorf@briya.org>
2024-10-17 09:14:42 -0400

    Update script2.js

--- script2.js
+++ script2.js
@@ -19,12 +19,12 @@ circle.addEventListener('click', () => {
     step1.style.display = 'none';
     step2.style.display = 'block';
     //step3.style.display = 'block';
-    //setTimeout((){
-        //circle.style.transition = 'all 2s ease';
-        //circle.style.top = '0%';
-        //circle.style.left = '20%';
-        //circle.style.width = '0px';
-        //circle.style.height = '0px';
-    //}, 10000);
+    setTimeout((){
+        circle.style.transition = 'all 2s ease';
+        circle.style.top = '0%';
+        circle.style.left = '20%';
+        circle.style.width = '0px';
+        circle.style.height = '0px';
+    }, 10000);
   }   
 });
